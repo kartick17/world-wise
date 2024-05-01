9cbd62c3c4a9334ce13b9284f9bba80eb9214c19
kartick17 <karticksadhu0@gmail.com>
2024-05-01 21:02:59 +0530

    Set params and query string in url and get values from them

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
 import Home from './pages/Homepage'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
@@ -6,9 +8,9 @@ import PageNotFound from './pages/PageNotFound'
 import AppLayout from './pages/AppLayout'
 import Login from './pages/Login'
 import City from './components/City'
-import { useEffect, useState } from 'react'
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
+import Form from './components/Form'
 
 const BASE_URL = `http://localhost:8000`
 
@@ -42,19 +44,20 @@ function App() {
           <Route path='pricing' element={<Pricing />} />
           <Route path='login' element={<Login />} />
           <Route path='app' element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to='cities' />} />
             <Route
               path='cities'
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
             <Route
+              path='cities/:id'
+              element={<City cities={cities} isLoading={isLoading} />}
+            />
+            <Route
               path='countries'
               element={<CountryList cities={cities} isLoading={isLoading} />}
             />
-            <Route path='form' element={<p>FOrm</p>} />
+            <Route path='form' element={<Form />} />
           </Route>
           <Route path='*' element={<PageNotFound />} />
         </Routes>
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styles from './CityItem.module.css'
 
 const formatDate = (date) =>
@@ -8,14 +9,18 @@ const formatDate = (date) =>
   }).format(new Date(date))
 
 function CityItem({ city }) {
-  const { cityName, emoji, date, notes } = city
-  return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
+  const { cityName, emoji, date, notes, id, position } = city
+  const { lat, lng } = position
 
-      <button className={styles.deleteBtn}>&times;</button>
+  return (
+    <li>
+      <Link className={styles.cityItem} to={`${id}?lat=${lat}&lng=${lng}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   )
 }
