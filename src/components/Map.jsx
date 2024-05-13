519eb7b228a1e0b5d84e5a35433183c97e62c863
kartick17 <karticksadhu0@gmail.com>
2024-05-13 22:59:15 +0530

    Create useGeolocation hook and setting map positon with current location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,14 +11,19 @@ import {
 import styles from './Map.module.css'
 import { useEffect, useState } from 'react'
 import { useCities } from '../contexts/CitiesContext'
+import { useGeolocation } from '../hooks/useGeolocation'
+import Button from './Button'
 
 function Map() {
   const navigate = useNavigate()
   const { cities } = useCities()
-
   const [mapPosition, setMapPosition] = useState([40, 0])
-
   const [searchParams] = useSearchParams()
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeolocation()
 
   const mapLng = searchParams.get('lng')
   const mapLat = searchParams.get('lat')
@@ -30,8 +35,23 @@ function Map() {
     [mapLat, mapLng]
   )
 
+  useEffect(
+    function () {
+      if (geoLocationPosition) {
+        console.log(geoLocationPosition)
+        setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng])
+      }
+    },
+    [geoLocationPosition]
+  )
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
+    <div className={styles.mapContainer}>
+      {!geoLocationPosition && (
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your position'}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={8}
