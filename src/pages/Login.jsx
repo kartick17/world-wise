1e3a8ca88a98594c790acfb895bf86964b208721
kartick17 <kartick.sadhu@weloin.com>
2024-05-22 17:37:55 +0530

    Changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import styles from './Login.module.css'
 import { useAuth } from '../contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
+import Button from '../components/Button'
 
 export default function Login() {
   const navigate = useNavigate()
@@ -12,20 +13,23 @@ export default function Login() {
 
   useEffect(
     function () {
-      if (isAuthenticated) navigate('/app')
+      if (isAuthenticated)
+        navigate('/app', {
+          replace: true,
+        })
     },
     [isAuthenticated]
   )
 
-  function handleClick(e) {
+  function handleSubmit(e) {
     e.preventDefault()
 
-    login(email, password)
+    if (email && password) login(email, password)
   }
 
   return (
     <main className={styles.login}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor='email'>Email address</label>
           <input
@@ -47,7 +51,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button onClick={handleClick}>Login</button>
+          <Button type='primary'>Login</Button>
         </div>
       </form>
     </main>
